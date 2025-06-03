5d1e0c31897282361fbe0b17658c56bc4f0095b5
Anik Islam <anik54908@gmail.com>
2025-06-03 21:36:10 +0600

    create 4 section using tailwind css without responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,css,js}"],
   theme: {
     container: {
-      center: true, 
+      center: true,
     },
     extend: {
       colors: {
         "header-color": "#f9f9f9",
         "black-color": "#DC0083",
         "header-text-color": "#FABE4C",
+        "btn-gradient": "linear-gradient(180deg, #A4BC46 0%, #85A019 100%)",
+      },
+      screens: {
+        sixforty: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
     },
     fontfamiy: {
